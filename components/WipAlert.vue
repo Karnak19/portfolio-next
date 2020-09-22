7c95a531132db479afbc8a83db700afe786bba2d
Karnak19 <basile64.v@gmail.com>
2020-09-22 16:16:40 +0200

    Add WIP Component

---
+++ components/WipAlert.vue
@@ -0,0 +1,33 @@
+<template>
+  <vs-col
+    vs-type="flex"
+    vs-justify="center"
+    vs-align="center"
+    lg="8"
+    sm="12"
+    offset="2"
+  >
+    <vs-alert color="warn" gradient>
+      <template #icon>
+        <i class="bx bxs-info-circle"></i>
+      </template>
+      <template #title>Work in progress</template>
+      Hey ! Welcome on my new personnal website/portfolio ! Everything here is
+      in a <b>WIP</b> state. So expect to see a lot of changes every time you
+      jump in ! 🌈
+      <template #footer> Basile V. </template>
+    </vs-alert>
+  </vs-col>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+@media screen and (max-width: 900px) {
+  .vs-col--offset-2 {
+    margin-left: 0%;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,6 @@
 <template>
   <vs-row>
-    <vs-col
-      vs-type="flex"
-      vs-justify="center"
-      vs-align="center"
-      offset="4"
-      w="8"
-    >
-      100%
-    </vs-col>
+    <WipAlert />
   </vs-row>
 </template>
 
